581afff5abb172a9492d9cb706ce61420d4a5c8d
Lauren Walker <lauren.walker@duke.edu>
2025-02-11 13:34:07 -0500

    add tabs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// function for search bar
 function searchFunction() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("input");
@@ -16,3 +17,28 @@ function searchFunction() {
       }       
     }
   }
+
+// function for tabs
+function openPage(pageName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+  
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+  
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
